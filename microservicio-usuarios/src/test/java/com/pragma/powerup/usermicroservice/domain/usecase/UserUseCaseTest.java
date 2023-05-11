47270007e6515f24f82b9c83adc5ba7cdd2dade0
Miguel Zuluaga <migueldevep@gmail.com>
2023-05-10 20:57:41 -0500

    Unit tests are uploaded in the user microservice

---
+++ microservicio-usuarios/src/test/java/com/pragma/powerup/usermicroservice/domain/usecase/UserUseCaseTest.java
@@ -0,0 +1,48 @@
+package com.pragma.powerup.usermicroservice.domain.usecase;
+
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.RoleEntity;
+import com.pragma.powerup.usermicroservice.domain.api.IUserServicePort;
+import com.pragma.powerup.usermicroservice.domain.model.Role;
+import com.pragma.powerup.usermicroservice.domain.model.User;
+import com.pragma.powerup.usermicroservice.domain.spi.IUserPersistencePort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class UserUseCaseTest {
+
+    @Mock
+    private IUserPersistencePort userPersistencePort;
+
+    @InjectMocks
+    private UserUseCase userUseCase;
+
+    private User user;
+    private Role role;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        role = new Role(1L, "ROLE_OWNER","ROLE_OWNER");
+        user = new User(1L,"Juan","Perez","123","123456","12-12-2001","email@gamil.com","123",role);
+    }
+
+    @Test
+    void findUserById() {
+        when(userPersistencePort.findUserById(1L)).thenReturn(user);
+        assertNotNull(userUseCase.findUserById(1L));
+    }
+
+    @Test
+    void findUserByDni() {
+        when(userPersistencePort.findUserByDni("123")).thenReturn(user);
+        assertNotNull(userUseCase.findUserByDni("123"));
+    }
+
+}
